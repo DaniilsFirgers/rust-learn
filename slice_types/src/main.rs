34866d71f8f0ff55bffe43994bc96f35295c0f18
Daniils Firgers <dfirger@gmail.com>
2023-07-29 18:29:14 +0300

    finish slices

---
+++ slice_types/src/main.rs
@@ -0,0 +1,26 @@
+fn main() {
+    let s = String::from("hello world");
+
+    let len = s.len();
+    let _slice = &s[3..len]; // intentionally unused
+    let _slice1 = &s[..]; // or &s[0..len]
+
+    let first_word_str = first_word(&s);
+    println!("first word {first_word_str}")
+
+    // array slices
+    let array1 = [1,2,3,4,5];
+    let slice = &array1[1..3];
+
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+    &s[..]
+}
