a0ad45473e3f6e7c762c8283c8eac35fb00b3716
Daniils Firgers <dfirger@gmail.com>
2023-08-01 23:06:05 +0300

    add tmp solution for cli arguments

--- first-five-lessons-app/src/main.rs
+++ first-five-lessons-app/src/main.rs
@@ -1,18 +1,21 @@
+mod structs;
+
 use colored::*;
 use std::env;
 use std::process;
+use structs::CommandLineOutput;
 
 const VALID_NUM_OF_ARGS: i32 = 3;
 
 fn main() {
-    read_command_line()
+    let arguments = read_command_line();
 }
 
-fn read_command_line() {
+fn read_command_line() -> CommandLineOutput {
     let args: Vec<String> = env::args().collect();
     let args_len: usize = args.len() - 1;
 
-    if args_len != VALID_NUM_OF_ARGS.try_into().unwrap() {
+    if args_len as i32 != VALID_NUM_OF_ARGS {
         eprintln!(
             "Provided {} arguments instead of {}",
             args_len.to_string().red(),
@@ -21,12 +24,13 @@ fn read_command_line() {
         process::exit(1);
     }
 
-    let amount = &args[1];
-    let first_currency = &args[2];
-    let second_currency = &args[3];
+    let amount = args[1].parse::<f64>().unwrap();
+    let first_currency = args[2].parse::<f64>().unwrap();
+    let second_currency = args[3].parse::<f64>().unwrap();
 
-    println!(
-        "Amount  is {}, currency 1 is {}, currency 2 is {}",
-        amount, first_currency, second_currency
-    );
+    CommandLineOutput {
+        amount,
+        foreign_currency: first_currency,
+        home_currency: second_currency,
+    }
 }
---
+++ first-five-lessons-app/src/structs.rs
@@ -0,0 +1,5 @@
+pub struct CommandLineOutput {
+    pub amount: f64,
+    pub foreign_currency: f64,
+    pub home_currency: f64,
+}
