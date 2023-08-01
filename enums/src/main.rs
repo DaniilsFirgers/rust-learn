8fa1ef39c0cbb71e541ea74df7503c377cea6e70
Daniils Firgers <dfirger@gmail.com>
2023-08-01 18:38:54 +0300

    fix bugs in enums

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,30 +1,31 @@
 enum IpAddr {
-    V4(String)
-    V6(String)
+    V4(String),
+    V6(String),
 }
 
 enum Message {
     Quit,
-    Move { x: i32, y: i32},
+    Move { x: i32, y: i32 },
     Write(String),
-    ChangeColor(i32, i32,i32),
+    ChangeColor(i32, i32, i32),
 }
 
 fn main() {
-   let four = IpAddr::V4;
-   let six IpAddr::V6;
+    let four = IpAddr::V4;
+    let six = IpAddr::V6(String::from("::1"));
 
-   let home = IpAddr::V4(String::from("127.0.0.1"));
-   let loopback = IpAddr::V6(String:from("::1"))
+    let home = IpAddr::V4(String::from("127.0.0.1"));
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
 }
 
 // methods on enum using impl
 
 impl Message {
-    fn call(&Message){
+    fn call(&self) {
         // method body would be defined here
     }
 }
 // TADAM
-let m = Message::Write(String::from("hello"));
-m.call()
