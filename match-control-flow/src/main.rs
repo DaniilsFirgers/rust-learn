e2cbd22ed85aa7ff069dd0319c679ae63bffc065
Daniils Firgers <dfirger@gmail.com>
2023-08-05 22:31:49 +0300

    add match control flow section

---
+++ match-control-flow/src/main.rs
@@ -0,0 +1,60 @@
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn main() {
+    let five = plus_one(5);
+    let none = plus_one(None);
+    println!("{} - {}", five, none);
+}
+
+fn value_to_cents(coin: Coin)-> u8 {
+    match coin {
+        Coin::Penny=>{
+            println!("Lucky penny"),
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coinm::Quarter(state) =>{
+            println!("State quarter from {:?}!", state);
+        },
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i+1),
+    }
+}
+
+// catch all pattern 
+
+let dice_roll = 9;
+// in this case we want to use other value in a called function later (cath all pattern)
+match dice_roll {
+    3 => add_fancy_hat(),
+    7 => remove_fancy_hat(),
+    other => move_player(other),
+}
+
+// here we use _ as we do not bind to that value
+match dice_roll_reroll {
+    3 => add_fancy_hat(),
+    7 => remove_fancy_hat(),
+    _ => reroll()
+}
+
+fn add_fancy_hat() {}
+fn remove_fancy_hat() {}
+fn move_player(num_spaces: u8) {}
+fn reroll() {}
